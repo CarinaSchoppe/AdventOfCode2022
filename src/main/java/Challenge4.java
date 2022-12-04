be7f38ea892bad9b7f4c107fcf9b7c9389208f0b
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2022-12-04 19:28:43 +0100

    commit

--- src/main/java/Challenge4.java
+++ src/main/java/Challenge4.java
@@ -1,11 +1,15 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Challenge4 {
 
-    public static void main(String[] args) {
-        var reader = new BufferedReader(new InputStreamReader(Challenge4.class.getResourceAsStream("challenge4.txt")));
-        var lines = reader.lines().toList();
-        System.out.println(lines);
+    public static void main(String[] args) throws IOException {
+
+        //read in the file "challenge4.txt" from the resources folder
+        var file = Challenge4.class.getResourceAsStream("challenge4.txt");
+        var reader = new BufferedReader(new java.io.InputStreamReader(file));
+        System.out.println(reader.readLine());
+
+
     }
 }
