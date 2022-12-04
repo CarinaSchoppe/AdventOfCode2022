d9997f653b063383135dcf3d2f6d00a4f2a6d34e
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2022-12-04 19:58:25 +0100

    commit

--- src/main/java/Challenge4.java
+++ src/main/java/Challenge4.java
@@ -8,8 +8,13 @@ public class Challenge4 {
         //read in the file "challenge4.txt" from the resources folder
         var file = Challenge4.class.getResourceAsStream("challenge4.txt");
         var reader = new BufferedReader(new java.io.InputStreamReader(file));
-        System.out.println(reader.readLine());
+        while (reader.ready()) {
+            var jobPair = reader.readLine().split(",");
+            var elf1 = new int[]{Integer.parseInt(jobPair[0].split("-")[0]), Integer.parseInt(jobPair[0].split("-")[1])};
+            var elf2 = new int[]{Integer.parseInt(jobPair[1].split("-")[0]), Integer.parseInt(jobPair[1].split("-")[1])};
 
+
+        }
 
     }
 }
