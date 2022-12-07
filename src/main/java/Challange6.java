6dcdb1abe5401d329bf971d22aecf4af805e0e28
Carina Sophie Schoppe <Carina.Schoppe2912@gmail.com>
2022-12-07 14:19:09 +0100

    commit

---
+++ src/main/java/Challange6.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Objects;
+
+public class Challange6 {
+
+    public static void main(String[] args) throws IOException {
+        var reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(Challange6.class.getResourceAsStream("challenge6.txt"))));
+        String line = reader.readLine();
+        var charArray = line.toCharArray();
+        var preAmount = 0;
+        var checkAmount = 14;
+        for (int i = 0; i < charArray.length; i++) {
+            var set = new HashSet<Character>();
+            for (int j = 0; j < checkAmount; j++) {
+                set.add(charArray[i + j]);
+            }
+            if (set.size() == checkAmount) {
+                preAmount += checkAmount;
+                break;
+            }
+            preAmount++;
+        }
+        System.out.println(preAmount);
+
+    }
+}
